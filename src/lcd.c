61d03d6d83c38f205908cfa09062687409c3200e
LifeEmu <77578380+LifeEmu@users.noreply.github.com>
2023-08-05 19:56:51 +0800

    Add code for LCD emulation

---
+++ src/lcd.c
@@ -0,0 +1,41 @@
+#include <stdint.h>
+
+
+#include "../inc/mmu.h"
+#include "../inc/lcd.h"
+
+
+// renders a buffer
+void renderBuffer(const uint8_t* buf) {
+	int row, col, bit, byte;
+	for( row = 0; row < LCD_HEIGHT; ++row ) {
+		for( col = 0; col < LCD_WIDTH; col += 8 ) {
+			byte = *(buf + LCD_WIDTH * row + col);
+			for( bit = 7; bit >= 0; ++bit ) {
+				// assume 1BPP
+				setPix(col + bit, row, byte & 0x01);
+				byte >>= 1;
+			}
+		}
+	}
+	updateDisp();
+}
+
+
+// renders current VRAM
+// memory must be initialized
+void renderVRAM(void) {
+	int row, col, bit, byte;
+	const uint8_t* base = (uint8_t*)(DataMemory - ROM_WINDOW_SIZE + VRAM_BASE);
+	for( row = 0; row < LCD_HEIGHT; ++row ) {
+		for( col = 0; col < LCD_WIDTH; col += 8 ) {
+			byte = *(base + VRAM_WIDTH * row + col);
+			for( bit = 7; bit >= 0; ++bit ) {
+				// assume 1BPP
+				setPix(col + bit, row, byte & 0x01);
+				byte >>= 1;
+			}
+		}
+	}
+	updateDisp();
+}
