ca2c4e10614caee90569aa98985e4253afcc50f5
LifeEmu <77578380+LifeEmu@users.noreply.github.com>
2024-12-09 11:47:49 +0800

    Adjust timer increment value (to improve keyboard responsiveness)

--- src/SFR/timer.h
+++ src/SFR/timer.h
@@ -7,7 +7,7 @@
 
 // timer counter increment step
 // will be added to timer counter when `updateTimer` is called
-#define TIMER_STEP 72
+#define TIMER_STEP 16	// Assume 1000+ updates per second
 
 
 // -------- core thread
